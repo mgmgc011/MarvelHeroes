60b9ad04b1dbc026fe1ea1e5e7a989d6db4cc8a9
Mingu Chu <mingu@mingus-macbook-pro.local>
2016-09-03 16:30:19 -0500

    Initial Commit

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Mingu Chu on 9/3/16.
+//  Copyright © 2016 Mingu Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
